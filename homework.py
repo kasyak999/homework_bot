4af5e1a8681439a91609aa74fce9b44cafdbe104
Максим <kasyak999@yandex.ru>
2024-08-18 16:07:05 +0300

    up

--- homework.py
+++ homework.py
@@ -44,7 +44,7 @@ def check_tokens():
 def send_message(bot, message):
     """Ответ бота."""
     try:
-        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        bot.send_message(chat_id=globals()['TELEGRAM_CHAT_ID'], text=message)
     except apihelper.ApiTelegramException as error:
         message = f'ошибка телграм бота {error}'
         raise Exception(message) from error
